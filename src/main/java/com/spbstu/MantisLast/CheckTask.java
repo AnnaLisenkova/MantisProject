24172f7b8ff7308c2c1f3d3e42b5e035291ccd99
Anna <nutochka2089@gmail.com>
2017-05-16 21:05:54 +0300

    jenkins

--- src/main/java/com/spbstu/MantisLast/CheckTask.java
+++ src/main/java/com/spbstu/MantisLast/CheckTask.java
@@ -16,33 +16,29 @@ public class CheckTask {
     @FindAll({@FindBy(xpath = "//td[@class='column-summary']")})
     List<WebElement> elements;
 
+    @FindBy(xpath="//td[@class='column-category']")
+    WebElement category;
+
+    @FindBy(xpath="//td[@class='column-severity']")
+    WebElement severity;
+
+    @FindBy(xpath="//td[@class='column-status']")
+    WebElement status;
+
+    @FindBy(xpath="//td[@class='column-summary']")
+    WebElement summary;
+
+
     public  List<WebElement> getList(){
         return elements;
     }
 
     public boolean checkCreatedTaskInList(Task task, String condition, String personInCondition) {
-        return (
-                elements.stream()
-                .anyMatch((row -> row.findElement(By.xpath("//td[@class='column-category']"))
-                        .getText()
-                        .equals(task.getCategory())))
-                &&elements.stream()
-                .anyMatch((row -> row.findElement(By.xpath("//td[@class='column-severity']"))
-                        .getText()
-                        .equals(task.getInfluence())))
-                &&elements.stream()
-                .anyMatch((row -> row.findElement(By.xpath("//td[@class='column-status']"))
-                        .getText()
-                        .contains(condition)))
-                &&elements.stream()
-                .anyMatch((row -> row.findElement(By.xpath("//td[@class='column-status']"))
-                        .getText()
-                        .contains(personInCondition)))
-                &&elements.stream()
-                .anyMatch((row -> row.findElement(By.xpath("//td[@class='column-summary']"))
-                        .getText()
-                        .equals(task.getSummary()))
-                ));
+        return (elements.stream().anyMatch((row -> row.findElement(By.xpath("//td[@class='column-category']")).getText().equals(task.getCategory())))
+                &&elements.stream().anyMatch((row -> row.findElement(By.xpath("//td[@class='column-severity']")).getText().equals(task.getInfluence())))
+                &&elements.stream().anyMatch((row -> row.findElement(By.xpath("//td[@class='column-status']")).getText().contains(condition)))
+                &&elements.stream().anyMatch((row -> row.findElement(By.xpath("//td[@class='column-status']")).getText().contains(personInCondition)))
+                &&elements.stream().anyMatch((row -> row.findElement(By.xpath("//td[@class='column-summary']")).getText().equals(task.getSummary()))));
     }
 
     public boolean checkWhetherTaskContainsInList(String expected) {
